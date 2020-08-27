e5cce582af79f65c1c5202ea20e2b1b0e5db6173
mlky1234 <edb1631@naver.com>
2020-08-27 14:20:33 +0900

    기능 추가

--- link.js
+++ link.js
@@ -2,20 +2,24 @@ const link1 = document.getElementById('menu_1');
 const link2 = document.getElementById('menu_2');
 const link3 = document.getElementById('menu_3');
 const link4 = document.getElementById('menu_4');
+const link5 = document.getElementById('menu_5');
 const title = document.getElementById('title');
 const mode = document.getElementById('floating_mode');
 
 function handleClick1(){//건강
     window.open("https://eduro.dge.go.kr/hcheck/index.jsp","_blank");
 }
-function handleClick2(){//밴드
+function handleClick2(){//BAND
     window.open("https://band.us/home","_blank");
 }
-function handleClick3(){//클래스 룸 
+function handleClick3(){//클래스룸 
     window.open("https://classroom.google.com/","_blank");
 }
 function handleClick4(){//도담도담
     window.open("http://dodam.b1nd.com/","_blank");
+}
+function handleClick5(){//SODA
+    window.open("https://takeup.co.kr/","_blank");
 }
 function handleTitleClick(){//제목 클릭 시
     location.reload();
@@ -26,6 +30,7 @@ function init(){
     link2.addEventListener('click',handleClick2);
     link3.addEventListener('click',handleClick3);
     link4.addEventListener('click',handleClick4);
+    link5.addEventListener('click',handleClick5);
     title.addEventListener('click',handleTitleClick);
 }
 init();
